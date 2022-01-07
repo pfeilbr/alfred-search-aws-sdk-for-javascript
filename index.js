0719756d548a8bd1fa996d1825e6f2c622bc70ef
Brian Pfeil <brian.pfeil@gmail.com>
2022-01-07 14:02:17 -0500

    removed caching of items

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
-'use strict';
-import alfy from 'alfy';
-import stringOccurrence from 'string-occurrence';
-import fs from 'fs';
+"use strict";
+import alfy from "alfy";
+import stringOccurrence from "string-occurrence";
+import fs from "fs";
 
-let items = alfy.cache.get('items')
-if (!items) {
-	console.log(`cache miss`);
-	const data = JSON.parse(fs.readFileSync('./data.json'));
+const data = JSON.parse(fs.readFileSync("./data.json"));
 
-	items = data.map(api => {
-		const url = `http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/${api.service}.html#${api.name}-property`;
-	
-		return {
-			title: api.name,
-			autocomplete: api.name,
-			subtitle: api.serviceFullName,
-			keywords: api.keywords,
-			arg: url,
-			quicklookurl: url,
-			icon: {
-				path: `./icons/${api.icon}.png`
-			}
-		};
-	});
-	
-	alfy.cache.set('items', items)
-}
+const items = data.map((api) => {
+  const url = `http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/${api.service}.html#${api.name}-property`;
 
+  return {
+    title: api.name,
+    autocomplete: api.name,
+    subtitle: api.serviceFullName,
+    keywords: api.keywords,
+    arg: url,
+    quicklookurl: url,
+    icon: {
+      path: `./icons/${api.icon}.png`,
+    },
+  };
+});
 
 const matcher = (input, items) => {
-	const tokens = input.trim().toLowerCase().split(' ');
+  const tokens = input.trim().toLowerCase().split(" ");
 
-	const result = items.filter(item => {
-		item.count = stringOccurrence(item.keywords, tokens);
+  const result = items.filter((item) => {
+    item.count = stringOccurrence(item.keywords, tokens);
 
-		return item.count > 0;
-	});
+    return item.count > 0;
+  });
 
-	return result.sort((a, b) => b.count - a.count);
+  return result.sort((a, b) => b.count - a.count);
 };
-
 
 alfy.output(matcher(alfy.input, items));
